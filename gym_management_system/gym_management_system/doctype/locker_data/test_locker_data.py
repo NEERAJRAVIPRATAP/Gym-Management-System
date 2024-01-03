64b172b435a2fb1bbfee3b31febc930adfba68fd
NEERAJRAVIPRATAP <pankaj.kumar@nestorbird.com>
2024-01-03 19:16:27 +0530

    Doctype created

---
+++ gym_management_system/gym_management_system/doctype/locker_data/test_locker_data.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Nestorbird_Trainee_Team and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLockerData(FrappeTestCase):
+	pass
