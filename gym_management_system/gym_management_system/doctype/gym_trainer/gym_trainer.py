2485686833a87e9fff77f66b2775738e0146a293
Anshul Thakur <anshul.thakur@nestorbird.com>
2024-01-04 10:52:23 +0530

    Changes done

--- gym_management_system/gym_management_system/doctype/gym_trainer/gym_trainer.py
+++ gym_management_system/gym_management_system/doctype/gym_trainer/gym_trainer.py
@@ -1,8 +1,20 @@
 # Copyright (c) 2024, Nestorbird_Trainee_Team and contributors
 # For license information, please see license.txt
 
-# import frappe
+from frappe.utils import getdate
+import re
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class GymTrainer(Document):
-	pass
+
+	def validate(self):
+		dob = getdate(self.dob)
+		current_date = getdate()
+		age_in_years = current_date.year - dob.year - ((current_date.month, current_date.day) < (dob.month, dob.day))
+		self.age =  age_in_years
+	
+
+
+
