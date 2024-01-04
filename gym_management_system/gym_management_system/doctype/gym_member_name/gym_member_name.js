f25db307f04661727fb02a6e35f28b6aa28b9a99
NEERAJRAVIPRATAP <neeraj.pratap@nestorbird.com>
2024-01-04 15:20:13 +0400

    Neeraj

--- gym_management_system/gym_management_system/doctype/gym_member_name/gym_member_name.js
+++ gym_management_system/gym_management_system/doctype/gym_member_name/gym_member_name.js
@@ -2,7 +2,25 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Gym Member Name', {
-	// refresh: function(frm) {
+ 
 
-	// }
+	validate: function (frm)
+	{
+		if (frm.doc.middle_name == null)
+		{
+			frm.set_value("full_name",frm.doc.first_name+" "+frm.doc.last_name)
+		}
+		else if (frm.doc.last_name == null)
+		{
+			frm.set_value("full_name",frm.doc.first_name+" "+frm.doc.middle_name)
+		}
+		else if (frm.doc.middle_name == null && frm.doc.last_name == null)
+		{
+			frm.set_value("full_name",frm.first_name)
+		}
+		else {
+			frm.set_value("full_name",frm.doc.first_name+" "+frm.doc.middle_name+" "+frm.doc.last_name)
+		}
+	  	
+	}
 });
---
+++ gym_management_system/gym_management_system/doctype/subscription_details/subscription_details.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2024, Nestorbird_Trainee_Team and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class SubscriptionDetails(Document):
+	pass
