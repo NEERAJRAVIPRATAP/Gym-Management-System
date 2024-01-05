5b4e9bb631295f240fd96230a9a4a4274ada518a
NEERAJRAVIPRATAP <pankaj.kumar@nestorbird.com>
2024-01-05 10:51:05 +0530

    ver 8

--- gym_management_system/gym_management_system/doctype/gym_member_list/gym_member_list.py
+++
@@ -1,8 +0,0 @@
-# Copyright (c) 2024, Nestorbird_Trainee_Team and contributors
-# For license information, please see license.txt
-
-# import frappe
-from frappe.model.document import Document
-
-class GymMemberList(Document):
-	pass
