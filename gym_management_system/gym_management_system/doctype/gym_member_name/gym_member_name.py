d1ab6a698321b481d612c4a526b3ebf3c48befb0
NEERAJRAVIPRATAP <neeraj.pratap@nestorbird.com>
2024-01-06 14:14:10 +0400

    Updated

--- gym_management_system/gym_management_system/doctype/gym_member_name/gym_member_name.py
+++ gym_management_system/gym_management_system/doctype/gym_member_name/gym_member_name.py
@@ -7,26 +7,7 @@ from frappe.model.document import Document
 
 class GymMemberName(Document):
 
-   ''' def send_email_notification(self, email_address, message):
-        frappe.sendmail(
-            recipients=self.doc.email_address,
-            message="We received Your details"
-        )
-	
-    def execute(self):
-        member = frappe.get_doc("Gym Member Name", doc.gym_member_name)
-
-        for item in doc.user_details:
-            item.full_name = member.full_name
-            item.email_address = member.email_address
-            item.contact_no = member.contact
-
-        # create a new document
-        doc = frappe.get_doc({
-            'doctype': 'Task',
-            'title': 'New Task'
-        })
-        doc.insert()'''
+    doc=frappe.get_doc("Gym Trainer","")
 
 
 
