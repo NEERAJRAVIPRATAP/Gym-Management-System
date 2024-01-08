b96f6445aa17790e32f5baa4cff520db00bbef3e
Veenamra-0246 <veenamra.raj@nestorbird.com>
2024-01-08 05:48:39 +0000

    Blocker coming

--- gym_management_system/gym_management_system/doctype/group_class/group_class.js
+++ gym_management_system/gym_management_system/doctype/group_class/group_class.js
@@ -2,6 +2,7 @@
 frappe.ui.form.on('Group Class', {
     after_save: function(frm) {
         frappe.msgprint("Assigned Class to Trainer");
+        promptAndSendEmail(frm);
     },
     after_insert: function(frm) {
         notifyTrainer(frm);
@@ -21,6 +22,55 @@ function notifyTrainer(frm) {
             if (r.message) {
                 frm.msgprint("Notification sent to Trainer!");
             }
+        }
+    });
+}
+
+function promptAndSendEmail(frm) {
+    frappe.prompt([
+        {
+            fieldtype: 'Data',
+            label: 'Email Address',
+            fieldname: 'email',
+            reqd: true
+        }
+    ], function(values) {
+        // Send email and show progress message
+        console.log("check error !")
+        console.log(frm,values);
+        // console.log(email);
+        sendEmailToGymMember(frm, values.email); // Corrected variable name
+    }, 'Enter Gym Member Email', 'Submit');
+}
+
+function sendEmailToGymMember(frm, gymMemberEmail) {
+    console.log(frm,values.email)
+    showProgressIndicator(frm, 'Sending email to Gym Member...');
+    frappe.call({
+        method: 'gym_management_system.gym_management_system.doctype.group_class.group_class.send_email_to_gym_member',
+        args: {
+            docname: frm.doc.name,
+            email: gymMemberEmail
+        },
+        callback: function(r) {
+            frm.dashboard.hide_progress();
+            if (r.message) {
+                frm.msgprint('Mail sent to Gym Member!');
+            }
+        }
+    });
+}
+
+function showProgressIndicator(frm, message) {
+    frm.dashboard.show_progress(__('Sending Email'), 0.5);
+    frappe.call({
+        method: 'gym_management_system.gym_management_system.doctype.group_class.group_class.send_email_to_gym_member',
+        args: {
+            // Add any additional arguments as needed
+        },
+        callback: function(r) {
+            frm.dashboard.hide_progress();
+            frm.msgprint(message);
         }
     });
 }
--- gym_management_system/gym_management_system/doctype/group_class/group_class.py
+++ gym_management_system/gym_management_system/doctype/group_class/group_class.py
@@ -1,3 +1,4 @@
+# File: gym_management_system/gym_management_system/doctype/group_class/group_class.py
 from frappe.model.document import Document
 import frappe
 
@@ -8,7 +9,9 @@ class GroupClass(Document):
 def send_notification_to_trainer(docname):
     try:
         group_class = frappe.get_doc("Group Class", docname)
+        print(docname)
         trainer_email = frappe.get_value("Gym Trainer", group_class.member_trainer, "email")
+        print(trainer_email)
 
         if trainer_email:
             subject = f"Group Class '{group_class.name}' Details"
@@ -21,10 +24,6 @@ def send_notification_to_trainer(docname):
             )
 
             frappe.msgprint("Notification sent to Trainer!")
-
-            # Update Gym Member List in Group Class DocType
-            update_gym_member_list(group_class)
-
             return True
         else:
             frappe.msgprint("Warning: Trainer email not found. Notification not sent.")
@@ -33,11 +32,27 @@ def send_notification_to_trainer(docname):
         frappe.msgprint(f"Error: {str(e)}")
         return False
 
-def update_gym_member_list(group_class):
-    # Add the trainer to the Gym Member List in the Group Class DocType
-    if group_class.member_trainer not in group_class.gym_member_list:
-        group_class.append("gym_member_list", {
-            "trainer": group_class.member_trainer
-        })
+@frappe.whitelist()
+def send_email_to_gym_member(docname, email):
+    try:
+        group_class = frappe.get_doc("Group Class", docname)
+        member_email = frappe.get_value("Gym Member", {"name": group_class.gym_member_name}, "email")
 
-    group_class.save(ignore_permissions=True)
+        if member_email:
+            subject = f"Group Class '{group_class.name}' Details"
+            message = f"Hello Gym Member {group_class.gym_member_name}!\n\nGroup Class '{group_class.name}' details:\nClass Type: {group_class.class_type}\nStart Time: {group_class.start_time}\nEnd Time: {group_class.end_time}"
+
+            frappe.sendmail(
+                recipients=[member_email],
+                subject=subject,
+                message=message
+            )
+
+            frappe.msgprint("Notification sent to Gym Member!")
+            return True
+        else:
+            frappe.msgprint("Warning: Gym Member email not found. Notification not sent.")
+            return False
+    except Exception as e:
+        frappe.msgprint(f"Error: {str(e)}")
+        return False
