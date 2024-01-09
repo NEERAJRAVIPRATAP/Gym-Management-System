6055cca9d3f57a783bfad867f6cdda6f76a964c2
Anshul Thakur <anshul.thakur@nestorbird.com>
2024-01-09 11:24:05 +0530

    trainer set in member and resolve confilts

--- gym_management_system/gym_management_system/doctype/gym_member_name/gym_member_name.js
+++ gym_management_system/gym_management_system/doctype/gym_member_name/gym_member_name.js
@@ -1,23 +1,15 @@
-// Copyright (c) 2024, Nestorbird_Trainee_Team and contributors
-// For license information, please see license.txt
-
 frappe.ui.form.on('Gym Member Name', {
-	validate: function (frm) {
-		if (frm.doc.last_name === null) {
-			frm.set_value("full_name", frm.doc.first_name)
-		}
-		else {
-			frm.set_value("full_name", frm.doc.first_name + " " + frm.doc.last_name);
-		}
-		
-	},
+    validate: function (frm) {
+        if (frm.doc.last_name === null) {
+            frm.set_value("full_name", frm.doc.first_name)
+        }
+        else {
+            frm.set_value("full_name", frm.doc.first_name + " " + frm.doc.last_name);
+        }
+    },
 
- 
-
+    after_save:function(frm){
+        frappe.new_doc('Gym Membership UI')
+    }
 });
-
-
-
-
-
 
